ca8148b1bf724efdda5c003ce5d79181f3d2d6ee
Angelishere <angelsara1804@gmail.com>
2024-04-09 18:17:25 +0530

    View Bus & Delete Bus created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import AddBus from './components/AddBus';
+import ViewBus from './components/ViewBus';
+import DeleteBus from './components/DeleteBus';
 
 function App() {
   return (
     <div>
       <AddBus/>
+      <ViewBus/>
+      <DeleteBus/>
     </div>
   );
 }
---
+++ src/components/ViewBus.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+
+const ViewBus = () => {
+    return (
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                                <div class="card">
+                                    <img src="..." class="card-img-top" alt="..." />
+                                        <div class="card-body">
+                                            <p class="card-text">Some</p>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                                <div class="card">
+                                    <img src="..." class="card-img-top" alt="..." />
+                                        <div class="card-body">
+                                            <p class="card-text">Some</p>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                                <div class="card">
+                                    <img src="..." class="card-img-top" alt="..." />
+                                        <div class="card-body">
+                                            <p class="card-text">jij</p>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                                <div class="card">
+                                    <img src="..." class="card-img-top" alt="..." />
+                                        <div class="card-body">
+                                            <p class="card-text">iji</p>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                                <div class="card">
+                                    <img src="..." class="card-img-top" alt="..." />
+                                        <div class="card-body">
+                                            <p class="card-text">ss</p>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                                <div class="card">
+                                    <img src="..." class="card-img-top" alt="..." />
+                                        <div class="card-body">
+                                            <p class="card-text">cc</p>
+                                        </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default ViewBus
